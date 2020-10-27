72772d9208a3fc770901f68beacd0147acfdbb43
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-10-27 10:38:05 +0900

    add study time setting

--- src/index.js
+++ src/index.js
@@ -7,26 +7,28 @@ import "./styles.css";
 
 
 const children = ({ remainingTime }) => {
-  const hours = Math.floor(remainingTime / 3600);
-  const minutes = Math.floor((remainingTime % 3600) / 60);
-  const seconds = remainingTime % 60;
+  const minutes = Math.floor(remainingTime / 60)
+  const seconds = remainingTime % 60
 
   return (
     <div className="timer">
       <div className="text">Remaining</div>
       <div className="value">
-        {hours}:{minutes}:{seconds}
+       {minutes}:{seconds}
       </div>
-      <div className="text">seconds</div>
+      <div className="text">minutes</div>
     </div>
   );
 };
 
-
 function App() {
   const [tag, setTag] = useState(false);
   const [yourID, setYourID] = useState();
+  const [role, setRole] = useState('');
+  const [study, setStudy] = useState(1500);
+  const [breakTime, setBreak] = useState(300);
   const socketRef = useRef();
+  const time=0;
   
   useEffect(() => {
     socketRef.current = io.connect("http://localhost:8000");
@@ -45,6 +47,20 @@ function App() {
     else socketRef.current.emit("timer stop sign", "timer stop!!");
   }
 
+  useEffect(()=>{
+    console.log(study);
+  },[study]);
+
+  function handleStudyTime(e){
+    console.log(e.target.value);
+    setStudy(e.target.value);
+    console.log("!",study);
+  }
+
+  function handleBreakTime(e){
+
+  }
+
   return (
     <div className="App">
       <h1>
@@ -55,7 +71,7 @@ function App() {
       <div className="timer-wrapper">
         <CountdownCircleTimer
           isPlaying={tag}
-          duration={70}
+          duration={study}
           colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
           onComplete={() => [true, 1000]}
         >
@@ -63,15 +79,29 @@ function App() {
         </CountdownCircleTimer>
       </div>
       <div>
+        <h4>Role</h4>
+        <button onClick={()=> setRole('client')}>Client</button>
+        <button onClick={()=> setRole('server')}>Server</button>
+      </div>
+      <div>
+        <input
+          type="number"
+          min="1"
+          max="60"
+          name="study"
+          onChange={handleStudyTime}
+        />
+        <input
+          type="number"
+          min="1"
+          max="60"
+          name="break"
+          onChange={handleBreakTime}
+        />
       <button onClick={()=> sendTimerSign(true)}>
         START
       </button>
-
     </div>
-      <p className="info">
-        Change component properties in the code filed on the right to try
-        difference functionalities
-      </p>
     </div>
   );
 }
