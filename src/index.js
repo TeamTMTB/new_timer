86559693937b80b11f12482daa396ed5deee8d66
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-10-29 18:20:16 +0900

    add break time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef, useCallback} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import ReactDOM from "react-dom";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import io from "socket.io-client";
@@ -6,31 +6,35 @@ import "./styles.css";
 
 
 
-const children = ({ remainingTime }) => {
-  const minutes = Math.floor(remainingTime / 60)
-  const seconds = remainingTime % 60
 
-  return (
-    <div className="timer">
-      <div className="text">Remaining</div>
-      <div className="value">
-       {minutes}:{seconds}
-      </div>
-      <div className="text">minutes</div>
-    </div>
-  );
-};
+
 
 function App() {
   const [tag, setTag] = useState(false);
   const [yourID, setYourID] = useState();
   const [role, setRole] = useState('');
   const [study, setStudy] = useState(1500);
-  const [key, setKey] = useState(0);
   const [breakTime, setBreak] = useState(300);
+  const [key, setKey] = useState(0);
   const socketRef = useRef();
+  const [remainingTime, setRemainingTime] = useState();
   const time=0;
   
+  const children = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60)
+    const seconds = remainingTime % 60
+  
+    return (
+      <div className="timer">
+        <div className="text">Remaining</div>
+        <div className="value">
+         {minutes}:{seconds}
+        </div>
+        <div className="text">minutes</div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     socketRef.current = io.connect("http://localhost:8000");
     socketRef.current.on("your id", (id) =>{
@@ -48,25 +52,16 @@ function App() {
     else socketRef.current.emit("timer stop sign", "timer stop!!");
   }
 
-  useEffect(()=>{
-    console.log(study);
-  },[study]);
-
-  const onChange = useCallback(
-    (e) => {
-      setStudy(e.target.value);
-      console.log("!",study);
-    },[study]);
-
-  const handleStudyTime=(e)=>{
+  function handleStudyTime(e){
     console.log(e.target.value);
-    setStudy(e.target.value);
-    setKey(key);
-    console.log("!",study);
+    setStudy(e.target.value * 60);
+    setRemainingTime(study);
+    setKey(!key);
   }
 
   function handleBreakTime(e){
-
+    setBreak(e.target.value*60000);
+    setKey(!key);
   }
 
   return (
@@ -79,19 +74,22 @@ function App() {
       <div className="timer-wrapper">
         <CountdownCircleTimer
           isPlaying={tag}
-          key={key}
           duration={study}
+          key={key}
           colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-          onComplete={() => [true, 1000]}
+          onComplete={() => {
+            return [true, breakTime] 
+          }}
         >
           {children}
+          
         </CountdownCircleTimer>
       </div>
-      <div>
+      {/* <div>
         <h4>Role</h4>
         <button onClick={()=> setRole('client')}>Client</button>
         <button onClick={()=> setRole('server')}>Server</button>
-      </div>
+      </div> */}
       <div>
         <input
           type="number"
